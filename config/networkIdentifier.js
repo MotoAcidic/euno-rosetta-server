5357b3964f72e91b56470d39ee481d1e8f30ac03
TFinch <nexusnow@protonmail.com>
2022-02-20 21:43:35 -0500

    t

--- config/networkIdentifier.js
+++ config/networkIdentifier.js
@@ -3,7 +3,7 @@ const RosettaSDK = require('rosetta-node-sdk');
 const Types = RosettaSDK.Client;
 
 const Blockchain = 'EunoPay';
-const Network = process.env.EUNO_NETWORK || 'livenet';
+const Network = 'livenet';
 const networkIdentifier = new Types.NetworkIdentifier(Blockchain, Network);
 
 module.exports = networkIdentifier;
--- src/eunopayIndexer.js
+++ src/eunopayIndexer.js
@@ -1,5 +1,6 @@
-const config = require('../config');
+const config = require('../config/index.js');
 const Indexer = require('./Indexer');
 
 const EunoPayIndexer = new Indexer(config.data);
+console.log('Made it past indexer and moving to src/indexer')
 module.exports = EunoPayIndexer;
