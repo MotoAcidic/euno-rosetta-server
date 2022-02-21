13f8e6c38945644cba728583ae0eaa2d51825207
TFinch <nexusnow@protonmail.com>
2022-02-20 23:10:22 -0500

    g

--- config/networkIdentifier.js
+++ config/networkIdentifier.js
@@ -4,6 +4,7 @@ const Types = RosettaSDK.Client;
 
 const Blockchain = 'EunoPay';
 const Network = 'livenet';
+console.log('stepping into the network ident')
 const networkIdentifier = new Types.NetworkIdentifier(Blockchain, Network);
-
+console.log('made it out of the ident')
 module.exports = networkIdentifier;
--- src/eunopaySyncer.js
+++ src/eunopaySyncer.js
@@ -3,6 +3,7 @@ const config = require('../config');
 const Syncer = require('./Syncer');
 const EunoPayIndexer = require('./eunopayIndexer');
 
+console.log('entered the syncer module.')
 const EunoPaySyncer = new Syncer(config.syncer, EunoPayIndexer);
 console.log('Made it past syncer')
 module.exports = EunoPaySyncer;
